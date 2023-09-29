6cb5e316209abf8260d083cb2f89d9ece06e1369
Axrich <116589250+Axrich@users.noreply.github.com>
2023-09-29 21:45:21 +0500

    Update GreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,21 +1,13 @@
     package school.mjc.stage0.conditions.task2;
     
-    public class GreatestNumberPrinter {
-        public void printGreatest(int first, int second) {
-            if (first > second) {
-                System.out.println("The greatest number is: " + first);
-            } else if (second > first) {
-                System.out.println("The greatest number is: " + second);
-            } else {
-                System.out.println("Both numbers are equal: " + first); 
-            }
-        }
-    
-        public static void main(String[] args) {
-            GreatestNumberPrinter printer = new GreatestNumberPrinter();
-            
-            int num1 = 12;
-            int num2 = 20;
-            printer.printGreatest(num1, num2);
+public class GreatestNumberPrinter {
+    public void printGreatest(int first, int second) {
+        if (first > second) {
+            System.out.println(first);
+        } else if (second > first) {
+            System.out.println(second);
+        } else {
+            System.out.println(first); // or System.out.println(second);
         }
     }
+}
