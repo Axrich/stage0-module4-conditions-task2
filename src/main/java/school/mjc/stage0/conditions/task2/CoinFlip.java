2a3687867f21931be9908b2ed52c372bb34b8ec5
Axrich <116589250+Axrich@users.noreply.github.com>
2023-09-29 21:45:48 +0500

    Update CoinFlip.java

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -3,16 +3,9 @@ package school.mjc.stage0.conditions.task2;
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
         if (from1UpTo1000 < 500) {
-            System.out.println("Орел");
+            System.out.println("Eagle");
         } else {
-            System.out.println("Хвост");
+            System.out.println("Tail");
         }
-    }
-
-    public static void main(String[] args) {
-        CoinFlip coinFlip = new CoinFlip();
-    
-        int randomNumber = 400;
-        coinFlip.throwCoin(randomNumber);
     }
 }
